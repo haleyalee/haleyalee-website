00257aff6b1418c6bcb8fdd46f5315c69f70b156
haleyalee <haleyalee@gmail.com>
2021-12-05 15:32:39 -0500

    Updating home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,14 @@ function Home() {
   return (
     <div className="container">
       <Nav />
+      <div className="py-3">
+        <h1>Hello! I'm Haley</h1>
+        {/* <p>
+          I am a UX &amp; Front-End Developer who loves to transform idea into reality. I am an
+          Electrical &amp; Computer Engineering alumna from Cornell University and currently,
+          a front-end developer at Cognizant.
+        </p> */}
+      </div>
 
     </div>
   )
